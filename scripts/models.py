9aad548f704530bb56b66c51b08cefa0ea901ce0
Felipe Colombelli <fcolombelli@inf.ufrgs.br>
2022-04-09 15:34:07 -0300

    Add keras tunner to run experiments and models

--- scripts/models.py
+++ scripts/models.py
@@ -5,63 +5,112 @@ from keras.layers.core import Dense, Activation, Dropout
 from tensorflow.keras import layers, optimizers, losses, metrics, Model
 from stellargraph.mapper import FullBatchNodeGenerator
 from stellargraph.layer import GAT
+from losses import categorical_focal_loss
 
 def get_gat_model(generator, output_dimention,
-                  num_neurons_per_layer=[64,64],
-                  activations=['elu', 'elu'],  output_activation='softmax',
-                  attention_heads=8, in_dropout=0.15, attention_dropout=0.15, 
-                  learning_rate=0.0001, loss_function='categorical_crossentropy'):
+                  possible_num_layers=[1,2,3],
+                  possible_gammas=[0,1,2],
+                  possible_num_neurons=[32,64,128],
+                  possible_in_dropouts=[0.0, 0.1, 0.2, 0.3],
+                  activations_function='elu',  output_activation='softmax',
+                  possible_attention_heads=[2,4,8], 
+                  attention_dropout=0, 
+                  possible_lrs=[0.0001, 0.0005, 0.001, 0.005],
+                  loss_function_weights = None):
 
-    gat = GAT(
-        layer_sizes=num_neurons_per_layer+[output_dimention],
-        activations=activations+[output_activation],
-        attn_heads=attention_heads,
-        generator=generator,
-        in_dropout=in_dropout,
-        attn_dropout=attention_dropout,
-        normalize=None,
-    )
+    # hp: hyperparameter (tunner)
+    def model_builder(hp):
 
-    x_inp, predictions = gat.in_out_tensors()
-    model = Model(inputs=x_inp, outputs=predictions)
+        loss_function = categorical_focal_loss(alpha=loss_function_weights, 
+            gamma=hp.Choice("gamma", values=possible_gammas))
 
-    model.compile(optimizer=optimizers.Adam(learning_rate=learning_rate), loss=loss_function,
-            metrics=["acc", metrics.AUC(curve="ROC", name="auc_roc"), 
-                    metrics.AUC(curve="PR", name="auc_pr"), 
-                    metrics.Precision(name="precision"), 
-                    metrics.Recall(name="recall")])
+        num_neurons_per_layer = []
+        num_layers = hp.Choice("num_layers", values=possible_num_layers)
+        for layer in range(num_layers):
+            num_neurons_per_layer.append(hp.Choice(f"layer_{layer}_units", 
+                values=possible_num_neurons))
 
-    return model
+        in_dropout = hp.Choice("dropout", values=possible_in_dropouts)
+        learning_rate = hp.Choice("learning_rate", values=possible_lrs)
+        attention_heads = hp.Choice("attention_heads", 
+                            values=possible_attention_heads)
+
+        activations = [activations_function]*num_layers
+        activations += [output_activation]
+
+        gat = GAT(
+            layer_sizes=num_neurons_per_layer+[output_dimention],
+            activations=activations,
+            attn_heads=attention_heads,
+            generator=generator,
+            in_dropout=in_dropout,
+            attn_dropout=attention_dropout,
+            normalize=None
+        )
+
+        x_inp, predictions = gat.in_out_tensors()
+        model = Model(inputs=x_inp, outputs=predictions)
+
+        model.compile(optimizer=optimizers.Adam(learning_rate=learning_rate), 
+                loss=loss_function,
+                metrics=["acc", metrics.AUC(curve="ROC", name="auc_roc"), 
+                        metrics.AUC(curve="PR", name="auc_pr"), 
+                        metrics.Precision(name="precision"), 
+                        metrics.Recall(name="recall")])
+        return model
+
+    return model_builder
+
 
 
 def get_mlp_model(output_dimention, input_dimension, 
-                  num_neurons_per_layer=[64,64],
-                  dropout=0.15, activations=['elu', 'elu'], 
+                  possible_num_layers=[1,2,3],
+                  possible_gammas=[0,1,2],
+                  possible_num_neurons=[32,64,128],
+                  possible_dropouts=[0.0, 0.1, 0.2, 0.3],
+                  activations_function='elu', 
                   output_activation='softmax',
-                  learning_rate=0.0001, 
-                  loss_function='categorical_crossentropy'):
+                  possible_lrs=[0.0001, 0.0005, 0.001, 0.005],
+                  loss_function_weights = None):
 
-    input_dim = input_dimension
+    # hp: hyperparameter (tunner)
+    def model_builder(hp):
 
-    model = Sequential()
-    model.add(Dense(num_neurons_per_layer[0], input_dim=input_dim))
-    model.add(Activation(activations[0]))
-    model.add(Dropout(dropout))
+        input_dim = input_dimension
 
-    if len(num_neurons_per_layer) > 1:
-        for i in range(1, len(num_neurons_per_layer)):
-            model.add(Dense(num_neurons_per_layer[i]))
-            model.add(Activation(activations[i]))
-            model.add(Dropout(dropout))
+        loss_function = categorical_focal_loss(alpha=loss_function_weights, 
+            gamma=hp.Choice("gamma", values=possible_gammas))
+
+        num_neurons_per_layer = []
+        num_layers = hp.Choice("num_layers", values=possible_num_layers)
+        for layer in range(num_layers):
+            num_neurons_per_layer.append(hp.Choice(f"layer_{layer}_units", 
+                values=possible_num_neurons))
+
+        dropout = hp.Choice("dropout", values=possible_dropouts)
+        learning_rate = hp.Choice("learning_rate", possible_lrs)
+
+        model = Sequential()
+        model.add(Dense(num_neurons_per_layer[0], input_dim=input_dim))
+        model.add(Activation(activations_function))
+        model.add(Dropout(dropout))
+
+        if len(num_neurons_per_layer) > 1:
+            for i in range(1, len(num_neurons_per_layer)):
+                model.add(Dense(num_neurons_per_layer[i]))
+                model.add(Activation(activations_function))
+                model.add(Dropout(dropout))
 
 
-    model.add(Dense(output_dimention))
-    model.add(Activation(output_activation))
+        model.add(Dense(output_dimention))
+        model.add(Activation(output_activation))
 
-    model.compile(loss=loss_function, optimizer=optimizers.Adam(learning_rate=learning_rate), 
-          metrics=["acc", metrics.AUC(curve="ROC", name="auc_roc"), 
-                    metrics.AUC(curve="PR", name="auc_pr"), 
-                    metrics.Precision(name="precision"), 
-                    metrics.Recall(name="recall")])
+        model.compile(loss=loss_function, 
+            optimizer=optimizers.Adam(learning_rate=learning_rate), 
+            metrics=["acc", metrics.AUC(curve="ROC", name="auc_roc"), 
+                        metrics.AUC(curve="PR", name="auc_pr"), 
+                        metrics.Precision(name="precision"), 
+                        metrics.Recall(name="recall")])
+        return model
     
-    return model
+    return model_builder
--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -3,11 +3,13 @@ from pickle import FALSE
 import os
 
 from numpy import percentile
+from sklearn.utils import shuffle
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import logging
 from keras.callbacks import EarlyStopping
 import tensorflow as tf
+import keras_tuner as kt
 
 tf.compat.v1.logging.set_verbosity(40)
 
@@ -25,22 +27,22 @@ from losses import categorical_focal_loss
 
 models_names = ["gat", "mlp"]  # In the order they are trained/evaluated
 
-neurons_each_layer = [128, 64]
-activations = ['elu', 'elu']
-output_activation='softmax'
-dropout=0.15
-learning_rate=0.0005
-
-#loss_function = 'categorical_crossentropy'
-loss_functions = {
-  "COAD": categorical_focal_loss(alpha=[1.6023, 0.6295, 0.8294, 1.7195], gamma=0),
-  "KIRC": categorical_focal_loss(alpha=[0.5148, 2.5242, 1.0868, 1.3491], gamma=0),
-  "LUAD": categorical_focal_loss(alpha=[0.4559, 1.0206, 1.5239, 5.8552], gamma=0)
+possible_num_layers = [1, 2, 3]
+possible_num_neurons = [32, 64, 128]
+activations_function = 'elu'
+output_activation = 'softmax'
+possible_dropouts = [0.0, 0.1, 0.2, 0.3]
+possible_lrs = [0.0001, 0.0005, 0.001, 0.005]
+possible_gammas = [0, 1, 2]
+loss_function_weights = {
+  "COAD": [1.6023, 0.6295, 0.8294, 1.7195],#categorical_focal_loss(alpha=[1.6023, 0.6295, 0.8294, 1.7195], gamma=0),
+  "KIRC": [0.5148, 2.5242, 1.0868, 1.3491],#categorical_focal_loss(alpha=[0.5148, 2.5242, 1.0868, 1.3491], gamma=0),
+  "LUAD": [0.4559, 1.0206, 1.5239, 5.8552]#categorical_focal_loss(alpha=[0.4559, 1.0206, 1.5239, 5.8552], gamma=0)
 }
 classes = ["stage1", "stage2", "stage3", "stage4"]
 
-attention_heads=8
-attention_dropout=0
+possible_attention_heads = [2, 4, 8]
+attention_dropout = 0
 
 mlp_batch_size=8
 
@@ -51,15 +53,18 @@ b = "C:/Users/colombelli/Desktop/TCC/experiments/"
 percentiles = ["001", "005", "01", "025", "05", "075", "09", "095", "099"]
 for cancer_type in ["KIRC", "COAD", "LUAD"]:
   for net_type in ["snf", "correlation", "correlation_multi_omics"]:
-    for p in percentiles:
+    for p in reversed[percentiles]:   #CHANGETHIS
       base_paths.append((cancer_type, f"{b}{cancer_type}/{net_type}/{p}/"))
 
 
 early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=50)
 training_epochs = 300
-repetitions = 10
+hp_epochs = 10
+hp_early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=0, patience=5)
+repetitions = 3   #CHANGETHIS
 experiments_seed = 42
 mlp_only_once = True
+train_mlp = True
 
 ##################################################
 ##################################################
@@ -74,10 +79,24 @@ from tensorflow import random
 random.set_seed(experiments_seed)
 
 
+from contextlib import contextmanager
+import sys, os
+
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:  
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+
 
 if __name__ =="__main__":
     
-
+  prev_cancer_type = None
   for cancer_type, base_path in base_paths:
     seed(experiments_seed)
     random.set_seed(experiments_seed)
@@ -89,7 +108,14 @@ if __name__ =="__main__":
     dm = DataManager(base_path, models_names, classes)
     df_patients, df_features, df_classes, G = dm.load_all_data(only_cancer=True)
 
-    train_mlp = True
+    if mlp_only_once:
+      if prev_cancer_type != cancer_type:
+        prev_cancer_type = cancer_type
+        train_mlp = True
+      else:
+        train_mlp = False
+
+
     for i in range(repetitions):
 
       X_train, X_test, y_train, y_test = train_test_split(df_features, 
@@ -116,11 +142,27 @@ if __name__ =="__main__":
       validation_gen = generator.flow(X_validation.index, y_validation)
       test_gen = generator.flow(X_test.index, y_test)
 
-      gat_model = get_gat_model(generator, y_train.shape[1], neurons_each_layer,
-        activations, output_activation, attention_heads, dropout, 
-        attention_dropout, learning_rate, loss_functions[cancer_type])
+      gat_model_builder = get_gat_model(generator, y_train.shape[1], 
+                    possible_num_layers, possible_gammas, possible_num_neurons,
+                    possible_dropouts, activations_function, output_activation,
+                    possible_attention_heads, attention_dropout, 
+                    possible_lrs, loss_function_weights[cancer_type])
 
-      
+      gat_tuner = kt.Hyperband(gat_model_builder,
+                     objective=kt.Objective('auc_pr', direction='max'),
+                     max_epochs=hp_epochs,
+                     directory=dm.base_path,
+                     project_name='hp_tuner_gat')
+
+      print("\nTuning GAT model...")
+      with suppress_stdout():
+        gat_tuner.search(train_gen, epochs=hp_epochs, shuffle=False,
+          validation_data=(validation_gen), callbacks=[hp_early_stop])
+        
+        best_hps = gat_tuner.get_best_hyperparameters(5)
+        # Build the model with the best hp.
+        gat_model = gat_model_builder(best_hps[0])
+
       print("\nTraining GAT model... ")
       gat_history = gat_model.fit(train_gen, epochs=training_epochs, 
           validation_data=(validation_gen), 
@@ -136,9 +178,28 @@ if __name__ =="__main__":
       preds.append(gat_pred)
       
       if train_mlp:
-        mlp_model = get_mlp_model(y_train.shape[1], X_train.values.shape[1], 
-          neurons_each_layer, dropout, activations, output_activation,
-          learning_rate, loss_functions[cancer_type])
+        mlp_model_builder = get_mlp_model(y_train.shape[1], 
+                              X_train.values.shape[1], possible_num_layers,
+                              possible_gammas, possible_num_neurons, 
+                              possible_dropouts, activations_function,
+                              output_activation, possible_lrs, 
+                              loss_function_weights[cancer_type])
+
+        mlp_tuner = kt.Hyperband(mlp_model_builder,
+                     objective=kt.Objective('auc_pr', direction='max'),
+                     max_epochs=hp_epochs,
+                     directory=dm.base_path,
+                     project_name='hp_tuner_mlp')
+
+        print("\nTuning MLP model...")
+        with suppress_stdout():
+          mlp_tuner.search(X_train, y_train, epochs=hp_epochs, 
+            validation_data=(X_validation, y_validation),
+            callbacks=[hp_early_stop], batch_size=mlp_batch_size)
+
+          best_hps = mlp_tuner.get_best_hyperparameters(5)
+          # Build the model with the best hp.
+          mlp_model = mlp_model_builder(best_hps[0])
 
         print("\nTraining MLP model...")
         mlp_history = mlp_model.fit(X_train, y_train, epochs=training_epochs, 
@@ -158,6 +219,3 @@ if __name__ =="__main__":
       
       print("\nSaving confusion matrices...\n")
       dm.save_conf_matrices(preds, y_test, i)
-
-      if mlp_only_once:
-        train_mlp = False
